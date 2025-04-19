6f9bf3cecb061185342de66c6188ed9b5da3a8f2
camilo-atb <camilotellez1821@gmail.com>
2025-04-19 14:32:34 -0500

    commit:"cel index"

--- script/script.js
+++ script/script.js
@@ -1,5 +1,17 @@
 const pass = document.getElementById("pass"), 
     icon = document.querySelector(".bx");
+
+const iconoHamburguesa = document.querySelector(".icono__hamburguesa");
+const menuNavegacion = document.querySelector(".menu__navegacion");
+
+function eventoIconoHamburguesa(e){
+    iconoHamburguesa.classList.toggle("icono__hamburguesa--active");
+    menuNavegacion.classList.toggle("menu__navegacion--active");
+}
+
+
+iconoHamburguesa.addEventListener("click", eventoIconoHamburguesa);
+
 icon.addEventListener("click", e => {
         if (pass.type === "password") {
             pass.type = "text";
@@ -7,3 +19,4 @@ icon.addEventListener("click", e => {
             pass.type = "password"
         }
 })
+
